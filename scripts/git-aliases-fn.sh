e1d4c0de9821a084a4ad9cfd1d6fcd752eeb25bd
Damien Amoury <damien.amoury@sopht.com>
2024-12-02 10:23:49 +0100

    Fix for Mac : function removeLocalOrphanBranch

--- scripts/git-aliases-fn.sh
+++ scripts/git-aliases-fn.sh
@@ -37,12 +37,14 @@ function grb() {
 }
 
 function removeLocalOrphanBranch() {
-  local remote=( $( git branch -r --format="%(refname:short)" | while read b; do echo '"'$b'"'; done ) ) 
-  local local=( $( git branch --format="%(refname:short)" | while read b; do echo '"'$b'"'; done ) )
+  git fetch -q --prune
+  local remote=($( git branch -r --format="%(refname:short)" | while read b; do echo '"'$b'"'; done )) 
+  local local=($( git branch --format="%(refname:short)" | while read b; do echo '"origin/'$b'"'; done ))
 
   for branch in "${local[@]}"; do
     if [[ $(echo ${remote[@]} | fgrep -cw "$branch") -eq 0 ]]; then
-      git branch -D $branch
+      local localBranchName=$(echo $branch | sed -E 's/"origin\/(.+)"/\1/g')
+      git branch -D $localBranchName
     fi
   done
 }
